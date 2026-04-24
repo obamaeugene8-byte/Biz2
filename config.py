1c3f7bf021aca248e91426885c221638caa716b9
obamaeugene8-byte <obamaeugene8@gmail.com>
2026-04-24 21:29:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
+
 class Config:
-    SECRET_KEY = "super-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///workload.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///app.db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
